368681c760eca2d41b5418481154c618fe491eb5
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-15 16:52:50 -0800

    added supabase url and key, started adding switch and route to app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, NavLink, Link, Switch, Route } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <p>
+          <NavLink exact to="/books">
+            Home
+          </NavLink>
+        </p>
+
+        <Switch>
+          <Route path="/books" component={BookList} />
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
